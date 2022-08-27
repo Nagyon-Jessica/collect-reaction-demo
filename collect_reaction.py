1c9455157c96dc5c83a6608701a34c345e6b1e2f
yagaodekawasu <40302128+Nagyon-Jessica@users.noreply.github.com>
2022-08-27 10:52:11 +0900

    Refactor

--- collect_reaction.py
+++ collect_reaction.py
@@ -16,11 +16,9 @@ def change_time_JST(u_time):
     u_time.hour,u_time.minute,u_time.second, tzinfo=timezone.utc)
     #タイムゾーンを日本時刻に変換
     jst_time_since = utc_time.astimezone(pytz.timezone("Asia/Tokyo"))
-    jst_time_until = jst_time_since + timedelta(minutes=10)
     # 文字列で返す
-    str_time_since = jst_time_since.strftime("%Y-%m-%d_%H:%M:%S_JST")
-    str_time_until = jst_time_until.strftime("%Y-%m-%d_%H:%M:%S_JST")
-    return (str_time_since, str_time_until)
+    str_time_since = jst_time_since.strftime("%Y-%m-%d_%H:%M:%S")
+    return str_time_since
 
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -54,7 +52,8 @@ while True:
         # RTしたユーザーの情報
         user = tweet.user
         # 検索範囲（開始，終了）
-        since, until = change_time_JST(tweet.created_at)
+        since = tweet.created_at.strftime("%Y-%m-%d_%H:%M:%S_UTC")
+        until = (tweet.created_at + timedelta(minutes=10)).strftime("%Y-%m-%d_%H:%M:%S_UTC")
         # 検索クエリ
         query = f"from:{user.screen_name} since:{since} until:{until} exclude:retweets"
         print(f"query: {query}")
@@ -63,13 +62,13 @@ while True:
         if len(ret2) > 0:
             # Tweet本文（改行文字を除去）
             text = ret2[-1]._json['text'].replace("\n", "")
-            # Tweet日時
-            ctime = ret2[-1]._json['created_at']
+            # Tweet日時(JST)
+            ctime = change_time_JST(ret2[-1].created_at)
             # 1行のDataFrameを作成してマスターに結合
             df2 = pd.DataFrame({'name': [user.name], 'screen_name': [user.screen_name], 'text': [text], 'created_at': [ctime]})
             df = pd.concat([df, df2])
-        # max_idの更新
-        max_id = id
+    # max_idの更新
+    max_id = ret[-1].id
 
 # DataFrameをCSVに出力
 df.to_csv('out.csv', index=False)
